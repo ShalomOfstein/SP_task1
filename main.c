95274fde50c557d70e699ace9866e8320e9625f3
Shalom <shalomofstein@gmail.com>
2024-01-24 23:42:13 +0200

    slash n

--- main.c
+++ main.c
@@ -26,7 +26,7 @@ int main() {
     
 
     // Print Armstrong numbers
-    printf("The Armstrong numbers are:");
+    printf("\nThe Armstrong numbers are:");
     for (int i = num1; i <= num2; ++i) {
         if (isArmstrong(i)==1) {
             printf(" %d", i);
